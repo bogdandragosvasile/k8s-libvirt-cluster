e7c2b4636a9028a46624f7e17f15d697b58aafb9
Bogdan Dragos <bogdan.dragos.gm@gmail.com>
2025-08-12 18:31:50 +0300

    updated main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,115 +1,299 @@
-terraform {
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "~> 0.7"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.0"
-    }
-  }
-}
+# Complete main.tf for Kubernetes cluster on Libvirt with Ubuntu 24.04 base image
+# This provisions: 2 load balancers, 3 control planes, 3 workers
+# Assumes cloud_init_user_data.tpl and cloud_init_network_config.tpl are in the same directory
+# Adjust variables, IPs, memory/vcpu, etc., as needed for your setup
 
 provider "libvirt" {
-  uri = "qemu:///system"
+  uri = "qemu:///system"  # Connect to local Libvirt
 }
 
-resource "libvirt_network" "k8s_network" {
-  name      = "k8s_net"
-  mode      = "nat"
-  addresses = ["172.16.16.0/24"]
-  autostart = true
+# Variables
+variable "kube_ssh_public_key" {
+  description = "SSH public key for VMs"
+  type        = string
 }
 
-resource "libvirt_volume" "debian-bookworm-nocloud-base" {
-  name   = "debian-bookworm-nocloud-base.qcow2"
-  pool   = "default"
-  source = "https://cloud.debian.org/images/cloud/bookworm/20250804-2194/debian-12-nocloud-amd64-20250804-2194.qcow2"
+variable "gateway" {
+  description = "Network gateway IP"
+  type        = string
+  default     = "172.16.16.1"
+}
+
+variable "dns" {
+  description = "DNS server IP"
+  type        = string
+  default     = "8.8.8.8"
+}
+
+# Base Ubuntu image (pre-downloaded)
+resource "libvirt_volume" "base" {
+  name   = "ubuntu-24.04-server-cloudimg-amd64.img"
+  source = "/var/lib/libvirt/images/ubuntu-24.04-server-cloudimg-amd64.img"  # Path to the new image
   format = "qcow2"
+  pool   = "default"
 }
 
+# --- Load Balancers (2 VMs) ---
 locals {
-  vms = [
-    { name = "loadbalancer1", role = "Load Balancer", ip = "172.16.16.51",  ram = 512,  vcpus = 1 },
-    { name = "loadbalancer2", role = "Load Balancer", ip = "172.16.16.52",  ram = 512,  vcpus = 1 },
-    { name = "kcontrolplane1", role = "Control Plane", ip = "172.16.16.101", ram = 2048, vcpus = 2 },
-    { name = "kcontrolplane2", role = "Control Plane", ip = "172.16.16.102", ram = 2048, vcpus = 2 },
-    { name = "kcontrolplane3", role = "Control Plane", ip = "172.16.16.103", ram = 2048, vcpus = 2 },
-    { name = "kworker1",      role = "Worker",         ip = "172.16.16.201", ram = 2048, vcpus = 2 },
-    { name = "kworker2",      role = "Worker",         ip = "172.16.16.202", ram = 2048, vcpus = 2 },
-    { name = "kworker3",      role = "Worker",         ip = "172.16.16.203", ram = 2048, vcpus = 2 }
+  lb_vms = [
+    { name = "loadbalancer1", ip = "172.16.16.51", memory = 1024, vcpu = 1 },
+    { name = "loadbalancer2", ip = "172.16.16.52", memory = 1024, vcpu = 1 }
   ]
 }
 
-resource "libvirt_volume" "vms" {
-  for_each        = { for vm in local.vms: vm.name => vm }
-  name            = "${each.key}.qcow2"
-  pool            = "default"
-  base_volume_id  = libvirt_volume.debian-bookworm-nocloud-base.id
-  format          = "qcow2"
+resource "libvirt_volume" "lb" {
+  count            = length(local.lb_vms)
+  name             = "${local.lb_vms[count.index].name}.qcow2"
+  base_volume_id   = libvirt_volume.base.id
+  pool             = "default"
+  size             = 10737418240  # 10 GB
 }
 
-resource "libvirt_domain" "vms" {
-  for_each = { for vm in local.vms: vm.name => vm }
-  name     = each.key
-  memory   = each.value.ram
-  vcpu     = each.value.vcpus
-
-  network_interface {
-    network_id = libvirt_network.k8s_network.id
-    addresses  = [each.value.ip]
-  }
-
-  disk {
-    volume_id = libvirt_volume.vms[each.key].id
-  }
-
-  cloudinit = libvirt_cloudinit_disk.ci[each.key].id
-}
-
-resource "libvirt_cloudinit_disk" "ci" {
-  for_each = { for vm in local.vms: vm.name => vm }
-  name           = "${each.key}-cloudinit.iso"
-  pool           = "default"
-  user_data      = data.template_file.user_data[each.key].rendered
-  network_config = data.template_file.network_config[each.key].rendered
-}
-
-data "template_file" "user_data" {
-  for_each = { for vm in local.vms: vm.name => vm }
+data "template_file" "lb_user_data" {
+  count    = length(local.lb_vms)
   template = file("${path.module}/cloud_init_user_data.tpl")
+
   vars = {
-    hostname   = each.key
+    hostname   = local.lb_vms[count.index].name
     public_key = var.kube_ssh_public_key
   }
 }
 
-data "template_file" "network_config" {
-  for_each = { for vm in local.vms: vm.name => vm }
+data "template_file" "lb_network_config" {
+  count    = length(local.lb_vms)
   template = file("${path.module}/cloud_init_network_config.tpl")
+
   vars = {
-    ip      = each.value.ip
-    gateway = "172.16.16.1"
-    dns     = "8.8.8.8"
+    ip      = local.lb_vms[count.index].ip
+    gateway = var.gateway
+    dns     = var.dns
   }
 }
 
-locals {
-  load_balancers = [for vm in local.vms : { name = vm.name, ip = vm.ip } if vm.role == "Load Balancer"]
-  control_planes = [for vm in local.vms : { name = vm.name, ip = vm.ip } if vm.role == "Control Plane"]
-  workers        = [for vm in local.vms : { name = vm.name, ip = vm.ip } if vm.role == "Worker"]
+resource "libvirt_cloudinit_disk" "lb" {
+  count          = length(local.lb_vms)
+  name           = "${local.lb_vms[count.index].name}-cloudinit.iso"
+  user_data      = data.template_file.lb_user_data[count.index].rendered
+  network_config = data.template_file.lb_network_config[count.index].rendered
+  pool           = "default"
 }
 
-resource "local_file" "ansible_inventory" {
-  content  = templatefile("${path.module}/inventory.tmpl", {
-    load_balancers = local.load_balancers
-    control_planes = local.control_planes
-    workers        = local.workers
-  })
+resource "libvirt_domain" "lb" {
+  count  = length(local.lb_vms)
+  name   = local.lb_vms[count.index].name
+  memory = local.lb_vms[count.index].memory
+  vcpu   = local.lb_vms[count.index].vcpu
+
+  cloudinit = libvirt_cloudinit_disk.lb[count.index].id
+
+  disk {
+    volume_id = libvirt_volume.lb[count.index].id
+  }
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+# --- Control Planes (3 VMs) ---
+locals {
+  cp_vms = [
+    { name = "kcontrolplane1", ip = "172.16.16.101", memory = 4096, vcpu = 2 },
+    { name = "kcontrolplane2", ip = "172.16.16.102", memory = 4096, vcpu = 2 },
+    { name = "kcontrolplane3", ip = "172.16.16.103", memory = 4096, vcpu = 2 }
+  ]
+}
+
+resource "libvirt_volume" "cp" {
+  count            = length(local.cp_vms)
+  name             = "${local.cp_vms[count.index].name}.qcow2"
+  base_volume_id   = libvirt_volume.base.id
+  pool             = "default"
+  size             = 21474836480  # 20 GB for control planes
+}
+
+data "template_file" "cp_user_data" {
+  count    = length(local.cp_vms)
+  template = file("${path.module}/cloud_init_user_data.tpl")
+
+  vars = {
+    hostname   = local.cp_vms[count.index].name
+    public_key = var.kube_ssh_public_key
+  }
+}
+
+data "template_file" "cp_network_config" {
+  count    = length(local.cp_vms)
+  template = file("${path.module}/cloud_init_network_config.tpl")
+
+  vars = {
+    ip      = local.cp_vms[count.index].ip
+    gateway = var.gateway
+    dns     = var.dns
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cp" {
+  count          = length(local.cp_vms)
+  name           = "${local.cp_vms[count.index].name}-cloudinit.iso"
+  user_data      = data.template_file.cp_user_data[count.index].rendered
+  network_config = data.template_file.cp_network_config[count.index].rendered
+  pool           = "default"
+}
+
+resource "libvirt_domain" "cp" {
+  count  = length(local.cp_vms)
+  name   = local.cp_vms[count.index].name
+  memory = local.cp_vms[count.index].memory
+  vcpu   = local.cp_vms[count.index].vcpu
+
+  cloudinit = libvirt_cloudinit_disk.cp[count.index].id
+
+  disk {
+    volume_id = libvirt_volume.cp[count.index].id
+  }
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+# --- Workers (3 VMs) ---
+locals {
+  worker_vms = [
+    { name = "kworker1", ip = "172.16.16.201", memory = 8192, vcpu = 4 },
+    { name = "kworker2", ip = "172.16.16.202", memory = 8192, vcpu = 4 },
+    { name = "kworker3", ip = "172.16.16.203", memory = 8192, vcpu = 4 }
+  ]
+}
+
+resource "libvirt_volume" "worker" {
+  count            = length(local.worker_vms)
+  name             = "${local.worker_vms[count.index].name}.qcow2"
+  base_volume_id   = libvirt_volume.base.id
+  pool             = "default"
+  size             = 32212254720  # 30 GB for workers
+}
+
+data "template_file" "worker_user_data" {
+  count    = length(local.worker_vms)
+  template = file("${path.module}/cloud_init_user_data.tpl")
+
+  vars = {
+    hostname   = local.worker_vms[count.index].name
+    public_key = var.kube_ssh_public_key
+  }
+}
+
+data "template_file" "worker_network_config" {
+  count    = length(local.worker_vms)
+  template = file("${path.module}/cloud_init_network_config.tpl")
+
+  vars = {
+    ip      = local.worker_vms[count.index].ip
+    gateway = var.gateway
+    dns     = var.dns
+  }
+}
+
+resource "libvirt_cloudinit_disk" "worker" {
+  count          = length(local.worker_vms)
+  name           = "${local.worker_vms[count.index].name}-cloudinit.iso"
+  user_data      = data.template_file.worker_user_data[count.index].rendered
+  network_config = data.template_file.worker_network_config[count.index].rendered
+  pool           = "default"
+}
+
+resource "libvirt_domain" "worker" {
+  count  = length(local.worker_vms)
+  name   = local.worker_vms[count.index].name
+  memory = local.worker_vms[count.index].memory
+  vcpu   = local.worker_vms[count.index].vcpu
+
+  cloudinit = libvirt_cloudinit_disk.worker[count.index].id
+
+  disk {
+    volume_id = libvirt_volume.worker[count.index].id
+  }
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+# Generate Ansible inventory.ini
+data "template_file" "inventory" {
+  template = file("${path.module}/inventory.tpl")
+
+  vars = {
+    lb1_ip        = libvirt_domain.lb[0].network_interface[0].addresses[0]
+    lb2_ip        = libvirt_domain.lb[1].network_interface[0].addresses[0]
+    cp1_ip        = libvirt_domain.cp[0].network_interface[0].addresses[0]
+    cp2_ip        = libvirt_domain.cp[1].network_interface[0].addresses[0]
+    cp3_ip        = libvirt_domain.cp[2].network_interface[0].addresses[0]
+    worker1_ip    = libvirt_domain.worker[0].network_interface[0].addresses[0]
+    worker2_ip    = libvirt_domain.worker[1].network_interface[0].addresses[0]
+    worker3_ip    = libvirt_domain.worker[2].network_interface[0].addresses[0]
+  }
+}
+
+resource "local_file" "inventory" {
+  content  = data.template_file.inventory.rendered
   filename = "${path.module}/../ansible/inventory.ini"
+}
+
+# Outputs
+output "vm_ips" {
+  value = {
+    loadbalancer1 = libvirt_domain.lb[0].network_interface[0].addresses[0]
+    loadbalancer2 = libvirt_domain.lb[1].network_interface[0].addresses[0]
+    kcontrolplane1 = libvirt_domain.cp[0].network_interface[0].addresses[0]
+    kcontrolplane2 = libvirt_domain.cp[1].network_interface[0].addresses[0]
+    kcontrolplane3 = libvirt_domain.cp[2].network_interface[0].addresses[0]
+    kworker1 = libvirt_domain.worker[0].network_interface[0].addresses[0]
+    kworker2 = libvirt_domain.worker[1].network_interface[0].addresses[0]
+    kworker3 = libvirt_domain.worker[2].network_interface[0].addresses[0]
+  }
+  description = "IPs of all provisioned VMs"
+}
+
+output "ansible_inventory_path" {
+  value = local_file.inventory.filename
+  description = "Path to generated Ansible inventory file"
 }
